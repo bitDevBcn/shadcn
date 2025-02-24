602dee7c163bf9165d579a02057ecc9a379671a9
bitDevBcn <jofuentesle@gmail.com>
2025-02-24 06:58:45 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+export default defineNuxtConfig({
+  alias: {
+    assets: "#rootDir/assets"
+  },
+  css: ['@/assets/styles/main.css'],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
